d3309c072513050e096a72504328f58aeeaf9400
dwzm0 <dwzm00gmail.com>
2023-03-16 14:18:29 +0200

    prod build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
